cc90801deebcc55b81ecfde07c84debe394ed795
Kirill Baranov <baranovk33@gmail.com>
2025-04-05 15:10:40 -0400

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/chess-keyboard/', // Replace with your actual repo name
+  base: '/chess-keyboard/' // This is crucial for GitHub Pages
 })
